fb9c4e9881f99186f4ab71f821b51ced6eb10214
caiot <caiot@DESKTOP-J8ULQKP>
2018-10-07 12:11:45 -0300

    Prototipo treino view OK

--- src/com/personalapp/bean/user/view/TreinoViewBean.java
+++ src/com/personalapp/bean/user/view/TreinoViewBean.java
@@ -27,25 +27,36 @@ public class TreinoViewBean implements Serializable {
 	private final AlunoDao alunoDao;
 	private final ExternalContext externalContext;
 	private Aluno aluno;
-	private String email = "";
-	private int i;
+	private String email;
 	private Exercicio exercicio = new Exercicio();
 	private List<Exercicio> exeA;
 
-	public List<Exercicio> getExeA() {
-		return exeA;
-	}
-
-	public void setExeA(List<Exercicio> exeA) {
-		this.exeA = exeA;
-	}
-	
-	public void setI(int i) {
-		this.i = i;
+	@Inject
+	public TreinoViewBean(AlunoDao alunoDao, ExternalContext externalContext) {
+		this.externalContext = externalContext;
+		this.alunoDao = alunoDao;
+		this.aluno = alunoDao.findOne(this.email);
+		
 	}
 	
-	public int getI() {
-		return this.i;
+	@ExceptionHandler
+	public void init(String username) {
+		this.aluno = this.alunoDao.findAluno(username);
+		this.exeA = this.listExercicioA();
+	}
+	
+	public void searchTreino() throws IOException {
+		this.aluno = this.alunoDao.findOne(this.email);
+		this.exeA = this.listExercicioA();
+	}
+	
+	public void setEmail(String email) throws IOException {
+		this.email = email;
+		this.searchTreino();
+	}
+	
+	public String getEmail() {
+		return this.email;
 	}
 	
 	public void setExercicio(Exercicio exercicio) {
@@ -56,51 +67,12 @@ public class TreinoViewBean implements Serializable {
 		return this.exercicio;
 	}
 	
-
-	@Inject
-	public TreinoViewBean(AlunoDao alunoDao, ExternalContext externalContext) {
-		this.externalContext = externalContext;
-		this.alunoDao = alunoDao;
-		this.aluno = alunoDao.findOne("caio.torres@ccc.ufcg.edu.br");
-		
-	}
-	
-	@ExceptionHandler
-//	@PostConstruct
-	public void init(String username) {
-		this.aluno = this.alunoDao.findAluno(username);
-		this.exeA = this.listExercicioA();
-	}
-	
-//	@PostConstruct
-//	public void relist() {
-//		this.exeA = this.containsTreino("A") ? listExercicioA() : null;
-//	}
-	
-	@PostConstruct
-	public void searchTreino() throws IOException {
-//		if (this.email == null || this.email.trim().equals("")) {
-//			this.externalContext.redirect(this.externalContext.getApplicationContextPath());
-//		} else {
-			this.aluno = this.alunoDao.findOne("caio.torres@ccc.ufcg.edu.br");
-			this.exeA = this.listExercicioA();
-//		}
-	}
-	
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
-	public String getEmail() {
-		return this.email;
-	}
-	
 	public boolean containsTreino(String letter) {
-		if (this.aluno.getFichaDeTreino() == null) {
+		if (this.aluno == null || this.aluno.getFichaDeTreino() == null) {
 			return false;
 		}
 		if (letter.toUpperCase().equals("A")) {
-			return this.alunoDao.findOne("caio.torres@ccc.ufcg.edu.br").getFichaDeTreino().getTreinoA() != null;
+			return this.alunoDao.findOne(this.email).getFichaDeTreino().getTreinoA() != null;
 		}
 		if (letter.toUpperCase().equals("B")) {
 			return this.aluno.getFichaDeTreino().getTreinoB() != null;
@@ -111,12 +83,29 @@ public class TreinoViewBean implements Serializable {
 		if (letter.toUpperCase().equals("D")) {
 			return this.aluno.getFichaDeTreino().getTreinoD() != null;
 		} return this.aluno.getFichaDeTreino().getTreinoE() != null;
-
+	}
+	
+	public String getObs(String letter) {
+		if (letter.equalsIgnoreCase("A")) {
+			System.out.println(this.aluno.getFichaDeTreino().getTreinoA().getObs());
+			return this.aluno.getFichaDeTreino().getTreinoA().getObs().trim();
+		}
+		else if (letter.equalsIgnoreCase("B")) {
+			return this.aluno.getFichaDeTreino().getTreinoB().getObs().trim();
+		}
+		else if (letter.equalsIgnoreCase("C")) {
+			return this.aluno.getFichaDeTreino().getTreinoC().getObs().trim();
+		}
+		else if (letter.equalsIgnoreCase("D")) {
+			return this.aluno.getFichaDeTreino().getTreinoD().getObs().trim();
+		}
+		else {
+			return this.aluno.getFichaDeTreino().getTreinoE().getObs().trim();
+		}
 	}
 
 	public List<Exercicio> listExercicioA() {
-		this.exeA = gerador.buildListExerciciosA(this.alunoDao.findOne("caio.torres@ccc.ufcg.edu.br").getFichaDeTreino());
-		return this.exeA;
+		return gerador.buildListExerciciosA(this.alunoDao.findOne(this.email).getFichaDeTreino());
 	}
 	public List<Exercicio> listExercicioB() {
 		return gerador.buildListExerciciosB(this.aluno.getFichaDeTreino());
